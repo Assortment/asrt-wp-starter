af0d6289b02d5d3b9779fdabd8b68e577792be2a
lukewhitehouse <luke@lukewhitehouse.co.uk>
2015-08-16 17:16:50 +0100

    update gravity forms components

--- themes/yourtheme/lib/theme.php
+++ themes/yourtheme/lib/theme.php
@@ -59,3 +59,35 @@ if ( !is_admin() ) add_action("wp_enqueue_scripts", "wpst_requeue_jquery", 11);
 
 get_option("medium_crop") === false ? add_option("medium_crop", "1") : update_option("medium_crop", "1");
 get_option("large_crop") === false ? add_option("large_crop", "1") : update_option("large_crop", "1");
+
+
+
+/**
+ * Gravity forms
+ ******************************************************************************/
+
+/**
+ * Better validation message
+ */
+function wpst_update_validation_message( $message, $form ){
+    $output  = '<div class="alert alert--error">';
+    $output .= '<strong>Error:</strong> Please complete the required fields and try again';
+    $output .= '</div>';
+
+    return $output;
+}
+
+add_filter( 'gform_validation_message', 'wpst_update_validation_message', 10, 2 );
+
+/**
+ * Update the Form submit button
+ */
+function wpst_gforms_submit_button( $button, $form ){
+    $output  = '<button type="submit" class="btn btn--primary" id="gform_submit_button_' . $form["id"] . '">';
+    $output .= $btn_array["text"];
+    $output .= '</button>';
+
+    return $output;
+}
+
+add_filter( 'gform_submit_button', 'wpst_gforms_submit_button', 10, 2 );
