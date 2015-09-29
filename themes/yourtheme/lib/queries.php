435c2b1a21997b99d9a3aea4c5e4f7acbae13663
lukewhitehouse <luke@lukewhitehouse.co.uk>
2015-09-29 23:27:14 +0100

    add example queries

--- themes/yourtheme/lib/queries.php
+++ themes/yourtheme/lib/queries.php
@@ -9,9 +9,70 @@
  * refer mainly to times when you want to pool the database, usually
  * through a `WP_Query` or `query_posts` function.
  *
- * $. Example Post-type query
+ * $. Post type examples
  * $. Example Taxonomy query
  *
  */
 
 
+
+/**
+ * $. Post type examples
+ ******************************************************************************/
+
+/**
+ * Get pages.
+ *
+ * @param  int     $parent  Define the parent page of the pages you're getting.
+ * @return object           WP_Query instance
+ */
+function wpst_get_pages ( $parent = null ) {
+
+    // Define arguments for query.
+    $args = array(
+        'post_type'    => 'page',
+        'post_parent'  => 0,
+        'sort_order'   => 'ASC',
+        'sort_column'  => 'menu_order'
+    );
+
+    // If param is declared, override the 'post_parent' argument
+    if( !is_null($parent) ):
+        $args['post_parent'] = $parent;
+    endif;
+
+    // New instance of WP_Query class.
+    $output = new WP_Query( $args );
+
+    // Return the results
+    return $output;
+}
+
+
+
+/**
+ * $. Taxonomy examples
+ ******************************************************************************/
+
+/**
+ * Get category terms.
+ *
+ * @param  int   | Define the parent page of the pages you're getting.
+ * @return array | Array of Category objects.
+ */
+function wpst_get_categories ( $parent = null ) {
+
+    // Define arguments for query.
+    $args = array();
+
+    // If param is declared, add to arguments
+    if( !is_null($parent) ):
+        $args['parent'] = $parent;
+    endif;
+
+    // New instance of WP_Query class.
+    $output = get_categories( $args );
+
+    // Return the results
+    return $output;
+}
