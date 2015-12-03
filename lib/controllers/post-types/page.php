cd27fc381fd0bb848fb47ca59d532a51ede2148f
lukewhitehouse <luke@lukewhitehouse.co.uk>
2015-12-03 21:30:58 +0000

    Add number of posts param for pages query

--- lib/controllers/post-types/page.php
+++ lib/controllers/post-types/page.php
@@ -22,16 +22,18 @@
  * Get pages.
  *
  * @param  int     $parent  Define the parent page of the pages you're getting.
+ * @param  int     $count   Number of posts you'd like to bring through.
  * @return object           WP_Query instance
  */
-function wpst_get_pages ( $parent = null ) {
+function wpst_get_pages ( $parent = null, $count = -1 ) {
 
     // Define arguments for query.
     $args = array(
-        'post_type'    => 'page',
-        'post_parent'  => 0,
-        'order'        => $order_by,
-        'orderby'      => 'ID'
+        'post_type'      => 'page',
+        'post_parent'    => 0,
+        'posts_per_page' => $count,
+        'order'          => $order_by,
+        'orderby'        => 'ID'
     );
 
     // If param is declared, override the 'post_parent' argument
