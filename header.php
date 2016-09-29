ba0c4314353aa5a39b390910a180203a1ed0bffb
Luke Whitehouse <luke@lukewhitehouse.co.uk>
2016-09-29 11:18:25 +0100

    better template description for header.php

--- header.php
+++ header.php
@@ -7,7 +7,9 @@
  *
  * The header.php is created to house all the global information you need
  * within the site's header. For example, logo, tagline, navigation, etc.
- * This template is accessible via the get_header() WP function.
+ *
+ * This template is accessible via the get_header() WP function and
+ * includes the site's wp_head(); function
  *
  * - Meta Information
  * - Header
