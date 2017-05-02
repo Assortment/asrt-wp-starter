058552377fc20dfddf81ce5be73289281051900b
Jason Hobbs <purchases@thecodezombie.co.uk>
2017-05-02 16:50:58 +0100

    add commonly used functions

--- lib/utility/helpers.php
+++ lib/utility/helpers.php
@@ -18,7 +18,20 @@
  * Conditionals
  ******************************************************************************/
 
-// Conditionals here
+/**
+ * Check whether a given page is an ancestor of another
+ *
+ * @param  int   $id  The ID of the parent post
+ * @return bool       simple true/false
+ */
+function is_tree($pid) {
+    global $post;
+    if( is_page() && ($post->post_parent==$pid || is_page($pid)) ):
+        return true;
+    else:
+        return false;
+    endif;
+};
 
 
 /**
@@ -44,10 +57,42 @@ function wpst_get_excerpt_by_id ( $id ) {
 }
 
 /**
+ * Get a post's ACF image, with specific dimension
+ *
+ * @param  string  $key   The ACF key for the image
+ * @param string   $size  The image size, based on add_image_size();
+ * @param  int     $id    The ID of the post
+ *
+ * @return string         The full URL of the given ACF image
+ */
+function wpst_get_acf_image_url($key = 'image', $size = 'full', $id = '') {
+
+    if( $id == '' ):
+        global $post;
+        $id = $post->ID;
+    endif;
+
+    // default output false
+    $output = '';
+
+    // get ACF field
+    $image = get_field($key, $id);
+
+    // if field is present
+    if( !empty($image) ):
+        $output = !empty($image['sizes'][$size]) ? $image['sizes'][$size] : $image['url'];
+    else:
+        $output = false;
+    endif;
+
+    return $output;
+}
+
+/**
  * Adding a cache busting parameter of the theme's current
  * version to the end of a file.
  *
- * @param  string  $url  The URL of the file to modify
+ * @param   string  $url  The URL of the file to modify
  * @return  string  The updated file URL
  */
 function wpst_file_cache_busting ($url) {
